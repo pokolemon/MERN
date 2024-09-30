c3e9be9050358c71965a32d0decf39ee2ad983cb
Tushar <tushar.hasule@somaiya.edu>
2024-09-30 22:47:19 +0530

    change

--- server/db/db.js
+++ server/db/db.js
@@ -1,10 +1,18 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const connectToDatabase = async () => {
-    try{
-        await mongoose.connect(process.env.MONGODB_URL)
-    } catch(error) {
-        console.log(error)
+    try {
+        console.log(process.env.MONGODB_URL);
+        
+        await mongoose.connect(process.env.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB connected!");
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
     }
-}
-export default connectToDatabase
+};
+
+export default connectToDatabase;
--- server/index.js
+++ server/index.js
@@ -4,13 +4,16 @@ import cors from 'cors'
 import authRoutes from './routes/auth.js'
 import connectToDatabase from './db/db.js'
 
-connectToDatabase()
- const app = express()
- app.use(cors())
- app.use(express.json())
- app.use('/api/auth', authRoutes)
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.listen(process.env.PORT, ()=>{
+connectToDatabase()
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/auth', authRoutes)
+
+app.listen(process.env.PORT, () => {
     console.log(`server is Running on port ${process.env.PORT}`)
 
 })
